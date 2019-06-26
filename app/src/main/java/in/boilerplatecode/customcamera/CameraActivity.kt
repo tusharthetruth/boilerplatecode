37355c550d03f0592ec93cfcf2928b41194095f2
tushar nigam <tusharnigam88@gmail.com>
2019-06-26 14:47:50 +0530

    initial commit

---
+++ app/src/main/java/in/boilerplatecode/customcamera/CameraActivity.kt
@@ -0,0 +1,13 @@
+package `in`.boilerplatecode.customcamera
+
+import `in`.boilerplatecode.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
